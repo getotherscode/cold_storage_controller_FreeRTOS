c41ae227ad1ccf3c1afef23a359c62e1b3a13b0b
unknown <stevenapollo@163.com>
2026-05-17 07:37:09 +0800

    adc 采样计算

--- APP/include/adc_app.h
+++ APP/include/adc_app.h
@@ -4,7 +4,90 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-#define ADC_CHANNEL_NUM 12
+//adc sample channel and filter
+#define ADC_CHANNEL_NUM   12
+#define SAMPLE_BUFFER_NUM 40
+#define TRIM_NUMBER       15
+#define TABLE_SIZE 176
+
+static const int16_t TEMP_MIN = -50;
+static const int16_t TEMP_MAX = 125;
+
+
+// NTC ADC-T Table From deepseek convert the R-T table
+// R_fixed = 10kΩ，ADC 12位 (0~4095)，B=3380，25℃=10kΩ
+// range：-50℃ ~ 125℃
+const uint16_t adc_table[TABLE_SIZE] = 
+{
+    3987, 3980, 3973, 3965, 3956, 3949, 3939, 3931, // -50℃ ~ -43℃
+    3922, 3912, 3901, 3891, 3879, 3868, 3855, 3843, // -42℃ ~ -35℃
+    3829, 3815, 3801, 3786, 3771, 3755, 3738, 3720, // -34℃ ~ -27℃
+    3702, 3684, 3665, 3644, 3624, 3603, 3581, 3558, // -26℃ ~ -19℃
+    3535, 3511, 3486, 3461, 3435, 3408, 3380, 3352, // -18℃ ~ -11℃
+    3324, 3294, 3264, 3234, 3202, 3170, 3138, 3105, // -10℃ ~  -3℃
+    3071, 3037, 3002, 2967, 2932, 2896, 2859, 2822, //  -2℃ ~   5℃
+    2785, 2747, 2709, 2671, 2633, 2594, 2555, 2516, //   6℃ ~  13℃
+    2477, 2438, 2399, 2359, 2320, 2281, 2242, 2202, //  14℃ ~  21℃
+    2163, 2124, 2086, 2048, 2009, 1971, 1933, 1895, //  22℃ ~  29℃
+    1858, 1821, 1785, 1749, 1713, 1677, 1642, 1608, //  30℃ ~  37℃
+    1574, 1540, 1507, 1474, 1442, 1410, 1379, 1348, //  38℃ ~  45℃
+    1318, 1288, 1259, 1231, 1203, 1175, 1148, 1122, //  46℃ ~  53℃
+    1095, 1070, 1045, 1021,  997,  973,  950,  928, //  54℃ ~  61℃
+     906,  884,  863,  843,  823,  803,  784,  765, //  62℃ ~  69℃
+     747,  729,  711,  695,  678,  662,  646,  630, //  70℃ ~  77℃
+     615,  601,  586,  572,  559,  545,  532,  520, //  78℃ ~  85℃
+     508,  496,  484,  472,  461,  450,  440,  429, //  86℃ ~  93℃
+     419,  410,  400,  391,  382,  373,  364,  356, //  94℃ ~ 101℃
+     348,  340,  332,  324,  317,  310,  303,  296, // 102℃ ~ 109℃
+     289,  283,  276,  270,  264,  258,  252,  247, // 110℃ ~ 117℃
+     241,  236,  231,  226,  222,  217,  212,  207  // 118℃ ~ 125℃
+};
+
+typedef enum 
+{
+    ADC_IDX_PRESS_INTAKE    = 0,   // CH0  PA0  
+    ADC_IDX_CURRENT_2       = 1,   // CH1  PA1  
+    ADC_IDX_CURRENT_1       = 2,   // CH2  PA2  
+    ADC_IDX_CURRENT_3       = 3,   // CH3  PA3  
+    ADC_IDX_TEMP_STORAGE    = 4,   // CH5  PA5  
+    ADC_IDX_TEMP_DEF        = 5,   // CH6  PA6  
+    ADC_IDX_TEMP_INTAKE     = 6,   // CH7  PA7  
+    ADC_IDX_TEMP_EXHAUST    = 7,   // CH9  PB1  
+    ADC_IDX_TEMP_CONDEN     = 8,   // CH10 PB2  
+    ADC_IDX_TEMP_AMBIENT    = 9,   // CH11 PB10 
+    ADC_IDX_TEMP_SPRAY      = 10,  // CH15 PB11 
+    ADC_IDX_PRESS_EXHAUST   = 11,  // CH16 PB12 
+    ADC_IDX_MAX             = 12
+} ADC_Index_E;
+
+typedef struct 
+{
+    //press
+    uint16_t press_intake[SAMPLE_BUFFER_NUM];
+    uint16_t press_exhaust[SAMPLE_BUFFER_NUM];
+
+    //current
+    uint16_t current_1[SAMPLE_BUFFER_NUM];
+    uint16_t current_2[SAMPLE_BUFFER_NUM];
+    uint16_t current_3[SAMPLE_BUFFER_NUM];
+
+    //temperature
+    uint16_t temp_storage[SAMPLE_BUFFER_NUM];
+    uint16_t temp_defrost[SAMPLE_BUFFER_NUM];
+    uint16_t temp_intake[SAMPLE_BUFFER_NUM];
+    uint16_t temp_exhaust[SAMPLE_BUFFER_NUM];
+    uint16_t temp_condensing[SAMPLE_BUFFER_NUM];
+    uint16_t temp_ambient[SAMPLE_BUFFER_NUM];
+    uint16_t temp_spray[SAMPLE_BUFFER_NUM];
+    
+    uint16_t sample_count;
+}SAMPLE_BUFFER_ST;
+
+typedef struct
+{
+    uint16_t adc_buffer[ADC_CHANNEL_NUM];
+    uint16_t adc_filter_data[ADC_CHANNEL_NUM];
+}ADC_DATA_BUFFER_ST;
 
 void adc_task(void* pvParameters);
 
--- APP/source/adc_app.c
+++ APP/source/adc_app.c
@@ -3,24 +3,180 @@
 #include "portmacro.h"
 #include "projdefs.h"
 #include "stm32g0xx_hal_adc.h"
+#include "stdbool.h"
 
 TaskHandle_t adcTaskHandle = NULL;
 
-uint16_t g_adc_buffer[ADC_CHANNEL_NUM];
+static SAMPLE_BUFFER_ST sample_buffer_st;
+static ADC_DATA_BUFFER_ST adc_data_st;
 
-void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef *hadc)
+//DMA Normal Mode
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
 {
+    (void) hadc;
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    //if adcTaskHandle has high priority, xHigherPriorityTaskWoken is true
     vTaskNotifyGiveFromISR(adcTaskHandle, &xHigherPriorityTaskWoken);
+    //let scheduler switch to high priority task
     portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
+
+//sample in a circular buffer,data won't be until the sample is finished
+static bool adc_sample_task(void)
+{
+    if(sample_buffer_st.sample_count >= SAMPLE_BUFFER_NUM)
+    {
+        sample_buffer_st.sample_count = 0;
+        return true;
+    }
+
+    //pushed into sample buffer and wait to filtered
+    //press
+    sample_buffer_st.press_intake[sample_buffer_st.sample_count] = adc_data_st.adc_buffer[ADC_IDX_PRESS_INTAKE];
+    sample_buffer_st.press_exhaust[sample_buffer_st.sample_count] = adc_data_st.adc_buffer[ADC_IDX_PRESS_EXHAUST];
+    //current
+    sample_buffer_st.current_1[sample_buffer_st.sample_count] = adc_data_st.adc_buffer[ADC_IDX_CURRENT_1];
+    sample_buffer_st.current_2[sample_buffer_st.sample_count] = adc_data_st.adc_buffer[ADC_IDX_CURRENT_2];
+    sample_buffer_st.current_3[sample_buffer_st.sample_count] = adc_data_st.adc_buffer[ADC_IDX_CURRENT_3];
+    //temperature
+    sample_buffer_st.temp_storage[sample_buffer_st.sample_count] = adc_data_st.adc_buffer[ADC_IDX_TEMP_STORAGE];
+    sample_buffer_st.temp_defrost[sample_buffer_st.sample_count] = adc_data_st.adc_buffer[ADC_IDX_TEMP_DEF];
+    sample_buffer_st.temp_intake[sample_buffer_st.sample_count] = adc_data_st.adc_buffer[ADC_IDX_TEMP_INTAKE];
+    sample_buffer_st.temp_exhaust[sample_buffer_st.sample_count] = adc_data_st.adc_buffer[ADC_IDX_PRESS_EXHAUST];
+    sample_buffer_st.temp_condensing[sample_buffer_st.sample_count] = adc_data_st.adc_buffer[ADC_IDX_TEMP_CONDEN];
+    sample_buffer_st.temp_ambient[sample_buffer_st.sample_count] = adc_data_st.adc_buffer[ADC_IDX_TEMP_AMBIENT];
+    sample_buffer_st.temp_spray[sample_buffer_st.sample_count] = adc_data_st.adc_buffer[ADC_IDX_TEMP_SPRAY];
+
+    sample_buffer_st.sample_count++;
+
+    return false;
+}
+
+//temperature and pressure type data filter algrithom
+static uint16_t sort_and_median_average(uint16_t* buffer, uint16_t trim_num)
+{
+    //* insert sort
+    //algrithmn understanding: image that a row of students, start from the first one, ask another student "are you older than me" who in front of this
+    //if yes the asked students move back one step, if not, the asking student sit at the current position    
+    for(int i = 1; i < SAMPLE_BUFFER_NUM; i++)
+    {
+        uint16_t key = buffer[i];
+        uint16_t j = i - 1;
+        while(j >= 0 && buffer[j] > key)
+        {
+            buffer[j + 1] = buffer[j];
+            j--;
+        }
+        buffer[j + 1] = key;
+    }
+
+    //trim at both sides and get the average
+    uint32_t sum = 0;
+    uint16_t average;
+    for(int i = trim_num; i < SAMPLE_BUFFER_NUM - trim_num; i++)
+    {
+        sum += buffer[i];
+    }
+
+    average = sum / (SAMPLE_BUFFER_NUM - trim_num * 2);
+    return average;
+}
+
+//current type data algrithom
+static uint16_t sort_and_topk_average(uint16_t* buffer, uint16_t trim_num)
+{
+    //* insert sort
+    //algrithmn understanding: image that a row of students, start from the first one, ask another student "are you older than me" who in front of this
+    //if yes the asked students move back one step, if not, the asking student sit at the current position    
+    for(int i = 1; i < SAMPLE_BUFFER_NUM; i++)
+    {
+        uint16_t key = buffer[i];
+        uint16_t j = i - 1;
+        while(j >= 0 && buffer[j] > key)
+        {
+            buffer[j + 1] = buffer[j];
+            j--;
+        }
+        buffer[j + 1] = key;
+    }
+
+    //trim at both sides and get the average
+    uint32_t sum = 0;
+    uint16_t average;
+    for(int i = trim_num * 2; i < SAMPLE_BUFFER_NUM; i++)
+    {
+        sum += buffer[i];
+    }
+
+    average = sum / (SAMPLE_BUFFER_NUM - trim_num * 2);
+    return average;
+}
+
+//get the filter origin data
+static void adc_filter_task(void)
+{
+    //sample task is ready, sort and get the median number five times and do average calculation
+    adc_data_st.adc_buffer[ADC_IDX_PRESS_INTAKE] = sort_and_median_average(sample_buffer_st.press_intake,TRIM_NUMBER);
+    adc_data_st.adc_buffer[ADC_IDX_PRESS_EXHAUST] = sort_and_median_average(sample_buffer_st.press_exhaust,TRIM_NUMBER);
+
+    adc_data_st.adc_buffer[ADC_IDX_CURRENT_1] = sort_and_topk_average(sample_buffer_st.current_1,TRIM_NUMBER);
+    adc_data_st.adc_buffer[ADC_IDX_CURRENT_2] = sort_and_topk_average(sample_buffer_st.current_2,TRIM_NUMBER);
+    adc_data_st.adc_buffer[ADC_IDX_CURRENT_3] = sort_and_topk_average(sample_buffer_st.current_3,TRIM_NUMBER);
+
+    adc_data_st.adc_buffer[ADC_IDX_TEMP_STORAGE] = sort_and_median_average(sample_buffer_st.temp_storage,TRIM_NUMBER);
+    adc_data_st.adc_buffer[ADC_IDX_TEMP_DEF] = sort_and_median_average(sample_buffer_st.temp_defrost,TRIM_NUMBER);
+    adc_data_st.adc_buffer[ADC_IDX_TEMP_INTAKE] = sort_and_median_average(sample_buffer_st.temp_intake,TRIM_NUMBER);
+    adc_data_st.adc_buffer[ADC_IDX_TEMP_EXHAUST] = sort_and_median_average(sample_buffer_st.temp_exhaust,TRIM_NUMBER);
+    adc_data_st.adc_buffer[ADC_IDX_TEMP_CONDEN] = sort_and_median_average(sample_buffer_st.temp_condensing,TRIM_NUMBER);
+    adc_data_st.adc_buffer[ADC_IDX_TEMP_AMBIENT] = sort_and_median_average(sample_buffer_st.temp_ambient,TRIM_NUMBER);
+    adc_data_st.adc_buffer[ADC_IDX_TEMP_SPRAY] = sort_and_median_average(sample_buffer_st.temp_spray,TRIM_NUMBER);
+}
+
+//convertion: Steinhart-Hart Formula, 1/T = A + B * ln(R) + C * (ln(R))^3
+//B-value bigger means NTC is sensitive to temperature change
+//1. if do not have ntc manual, get A,B,C by 3 groups equations
+//2. if you have R-T table, better. you can get ADC-T table by R-T table through 
+//   ADC = (R_ntc / (R_ntc + R_fixed)) * ADC_max, R_ntc in R-T table, R_fixed = 10k
+//   ADC_MAX = 2^12 = 4096
+static void scale_transformation_temperture()
+{
+    
+
+}
+
+static void scale_transformation_pressure()
+{
+
+}
+
+static void scale_transformation_current()
+{
+
 }
 
 void adc_task(void* pvParameters)
 {
-    HAL_ADC_Start_DMA(&hadc1, (uint32_t*)g_adc_buffer, ADC_CHANNEL_NUM);
-
+    (void) pvParameters;
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+    const TickType_t xPeriod = pdMS_TO_TICKS(1);  // 1ms
     for(;;)
     {
+        //1ms cycle
+        xTaskDelayUntil(&xLastWakeTime, xPeriod);
+        //restart
+        HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_data_st.adc_buffer, ADC_CHANNEL_NUM);
+        //if not take the sigle, here will be suspended
+        //if take the sigle, pdTRUE -> pdFALSE
         ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+        if(adc_sample_task())
+        {
+            //filter
+            adc_filter_task();
+            
+            //scale transformation: origin sample data -> human readable
+            scale_transformation_temperture();
+        }
+        
     }
 }
