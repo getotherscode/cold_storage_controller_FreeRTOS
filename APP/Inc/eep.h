0bb29683941354f1c777173639e79d10a5e12305
cobb <stevenapollo@163.com>
2026-07-03 21:40:46 +0800

    eep write and read

--- APP/Inc/eep.h
+++ APP/Inc/eep.h
@@ -1,3 +1,27 @@
 #pragma once
 
-void eep_one_byte_test();
+#include "eep.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct
+{
+    uint16_t cur_buf_index;
+    uint16_t left_bytes;
+    uint8_t state;
+
+    bool is_busy;           //write task finished flag, can not be set
+} EEP_WRITE_ST;
+
+typedef enum
+{
+    EEP_WRITE_INIT = 0,
+    EEP_WRITE,
+    EEP_FINISH
+} EEP_STATE;
+
+bool eep_write(const uint16_t start_address, uint8_t* buffer, const uint16_t len);
+
+bool eep_read(const uint16_t start_address, uint8_t* buffer, const uint16_t len);
+
+void eep_test(void);
--- APP/Src/eep.c
+++ APP/Src/eep.c
@@ -1,28 +1,32 @@
 /*   EEP FEATURE   */
 //CHIP-SIZE: 512 bytes
 //PAGE-SIZE: 16
+//PAGE-NUM : 32
+//PAGE-HEAD-ADDR: 0, 0x10, 0x20, 0x30 ... 0x100, 0x110，0x120, ... 0x1F0
 
-#include <stdint.h>
+#include <string.h>
 #include "FreeRTOS.h"
 #include "task.h"
 #include "i2c.h"
 #include "log.h"
+#include "eep.h"
 
-//ID 0b1010, chip enable address 0b00x(x is for addressing), bit0 is R/W
-static const uint8_t EEP_ID = 0x0A;
+static const uint8_t EEP_ID   = 0x0A;  //ID 0b1010, chip enable address 0b00x(x is for addressing), bit0 is R/W
+static const uint16_t EEP_END = 511;   //address is from 0 - 511
 
-//address is from 0 - 511
-//return: write success or write fail
-bool eep_write_one_byte(const uint16_t address, const uint8_t data)
+static EEP_WRITE_ST eep_w_st;
+
+//TODO: use oscillscope to verify encapsulate is not influence the i2c driver wave
+static bool eep_write_init(const uint16_t start_address)
 {
-    if(address > 511)
+    if(start_address > EEP_END)
     {
         return false;
     }
 
     //device select code + W
     uint8_t dev_slct_code = EEP_ID << 4;
-    if(address >= UINT8_MAX)
+    if(start_address >= UINT8_MAX)
     {
         //bit0 - bit3 : 0010
         dev_slct_code += 2; 
@@ -37,59 +41,21 @@ bool eep_write_one_byte(const uint16_t address, const uint8_t data)
         return false;
     }
 
-    //address
-    if(!i2c_put_a_byte(address))
+        //address
+    if(!i2c_put_a_byte(start_address))
     {
         i2c_stop();
         return false;
     }
-
-    //data
-    if(!i2c_put_a_byte(data))
-    {
-        i2c_stop();
-        return false;
-    }
-
-    i2c_stop();
 
     return true;
 }
 
-bool eep_random_read_one_byte(const uint16_t address, uint8_t* ret)
+static bool eep_read_init(const uint16_t start_address)
 {
-    if(address > 511)
-    {
-        return false;
-    }
-
-    //device select code + W to get to target address
-    uint8_t dev_slct_code = EEP_ID << 4;
-    if(address >= UINT8_MAX)
-    {
-        //bit0 - bit3 : 0010
-        dev_slct_code += 2; 
-    }
-    // else bit0 - bit3 : 0000
-
-    i2c_start();
-    
-    if(!i2c_put_a_byte(dev_slct_code))
-    {
-        i2c_stop();
-        return false;
-    }
-
-    //address
-    if(!i2c_put_a_byte(address))
-    {
-        i2c_stop();
-        return false;        
-    }
-
     //device select code + R
-    dev_slct_code = EEP_ID << 4;
-    if(address >= UINT8_MAX)
+    uint8_t dev_slct_code = EEP_ID << 4;
+    if(start_address >= UINT8_MAX)
     {
         //bit0 - bit3 : 0011
         dev_slct_code += 3; 
@@ -108,13 +74,165 @@ bool eep_random_read_one_byte(const uint16_t address, uint8_t* ret)
         return false;
     }
 
-    i2c_get_a_byte(ret, false);
+    return true;
+}
+
+//return: write success or write fail
+bool eep_write_one_byte(const uint16_t start_address, const uint8_t data)
+{
+    if(!eep_write_init(start_address))
+    {
+        return false;
+    }
+
+    //data
+    if(!i2c_put_a_byte(data))
+    {
+        i2c_stop();
+        return false;
+    }
+
+    i2c_stop();
+
+    return true;
+}
+
+/*  - page write, max 16 bytes written in same page (could not beyond the page)
+    - use address A4-bit to determine if beyonded the page in one written
+*/
+bool eep_page_random_write(const uint16_t start_address, uint8_t* buffer, const uint8_t len)
+{
+    uint16_t start_bit4 = (start_address & 0x0010);
+    uint16_t end_address = (start_address + len - 1);
+    uint16_t end_bit4 = (end_address & 0x0010);
+    bool in_same_page = (start_bit4 == end_bit4);
+
+    if(len > 16)
+    {
+        return false;
+    }
+
+    if(end_address > EEP_END)
+    {
+        return false;
+    }
+
+    if(!in_same_page)
+    {
+        return false;
+    }
+
+    if(!eep_write_init(start_address))
+    {
+        return false;
+    }
+
+    for(uint8_t index = 0; index < len ; index++)
+    {
+        if(!i2c_put_a_byte(buffer[index]))
+        {
+            return false;
+        }
+    }
+
+    i2c_stop();
+
+    return true;
+}
+
+bool eep_write(const uint16_t start_address, uint8_t* buffer, const uint16_t len)
+{
+    switch(eep_w_st.state)
+    {
+        case EEP_WRITE_INIT:
+        {
+            uint16_t end_address = (start_address + len - 1);
+
+            if(end_address > EEP_END)
+            {
+                return false;
+            }
+
+            eep_w_st.cur_buf_index = 0;
+            eep_w_st.left_bytes = len;
+            eep_w_st.state = EEP_WRITE;
+        }
+        break;
+
+        case EEP_WRITE:
+        {
+            if(eep_w_st.left_bytes == 0)
+            {
+                eep_w_st.state = EEP_FINISH;
+                eep_w_st.is_busy = false;
+                break;
+            }
+
+            //cross page have limit
+            uint16_t cur_start_addr = start_address + eep_w_st.cur_buf_index;
+            uint16_t next_page_head = (cur_start_addr & 0x0010) + 0x10;
+            uint8_t cur_page_availble = next_page_head - cur_start_addr;
+            
+            if(cur_page_availble > eep_w_st.left_bytes)
+            {
+                cur_page_availble = eep_w_st.left_bytes;
+            }
+
+            uint8_t temp_buffer[16];
+            memcpy(temp_buffer, buffer + eep_w_st.cur_buf_index, cur_page_availble);
+
+            if(!eep_page_random_write(cur_start_addr, temp_buffer, cur_page_availble))
+            {
+                return false;
+            }
+            else
+            {
+                //update
+                eep_w_st.cur_buf_index += cur_page_availble;
+                eep_w_st.left_bytes -= cur_page_availble;
+            }
+        }
+        break;
+
+        case EEP_FINISH:
+        {
+            eep_w_st.cur_buf_index = 0;
+        }
+        break;
+    }
+
+    return true;
+}
+
+bool eep_read(const uint16_t start_address, uint8_t* buffer, const uint16_t len)
+{
+    uint16_t end_address = (start_address + len - 1);
+
+    if(end_address > EEP_END)
+    {
+        return false;
+    }
+    
+    if(!eep_write_init(start_address))
+    {
+        return false;
+    }
+
+    if(!eep_read_init(start_address))
+    {
+        return false;
+    }
+
+    for(uint16_t counter = 0; counter < len; counter++)
+    {
+        bool give_ack = (counter == (len - 1)) ? false : true;
+        i2c_get_a_byte(buffer + counter, give_ack);
+    }
 
     i2c_stop();
     
     return true;
 }
-
 
 
 /*byte read and byte write test*/
