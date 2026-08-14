ed14f06e38a55b8067ee3924d668be79ef14dda0
cobb <stevenapollo@163.com>
2026-08-14 17:48:42 +0800

    delete alarm relay
    alarm module forcus on alarm logic

--- app/inc/alarm.h
+++ app/inc/alarm.h
@@ -1,10 +1,3 @@
 #pragma once
 
-#include "stdint.h"
-
-#define ALARM_RELAY_OFF 0
-#define ALARM_RELAY_ON  1
-
-void alarm_ctrl(uint8_t state);
-
-void alarm_main(void *pvParameters);
+#include <stdint.h>
--- app/src/alarm.c
+++ app/src/alarm.c
@@ -1,23 +1,3 @@
 #include "alarm.h"
 #include "main.h"
 
-#define TRUN_ON_ALARM_RELAY()  HAL_GPIO_WritePin(ALARM_GPIO_Port, ALARM_Pin, GPIO_PIN_SET)
-#define TRUN_OFF_ALARM_RELAY() HAL_GPIO_WritePin(ALARM_GPIO_Port, ALARM_Pin, GPIO_PIN_RESET)
-#define TOGGLE_ALARM_RELAY() HAL_GPIO_TogglePin(ALARM_GPIO_Port, ALARM_Pin)
-
-void alarm_ctrl(uint8_t state)
-{
-    if(state == ALARM_RELAY_ON)
-    {
-        TRUN_ON_ALARM_RELAY();
-    }
-    else if(state == ALARM_RELAY_OFF)
-    {
-        TRUN_OFF_ALARM_RELAY();
-    }
-}
-
-void alarm_main(void *pvParameters)
-{
-    (void) pvParameters;
-}
